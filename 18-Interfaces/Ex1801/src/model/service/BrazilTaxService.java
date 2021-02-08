25e06d75ecec90a3db1b4ac2c1944a52be8a1571
Vinicius Soares <vinicius.gomes.dev@gmail.com>
2021-02-08 03:20:18 -0300

    Solution with Interface

--- 18-Interfaces/Ex1801/src/model/service/BrazilTaxService.java
+++ 18-Interfaces/Ex1801/src/model/service/BrazilTaxService.java
@@ -1,8 +1,8 @@
 package model.service;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService{
 	
-	public Double tax(double amount) {
+	public double tax(double amount) {
 		return (amount <= 100.00) ? amount * 0.2 : amount * 0.15;
 	}
 
---
+++ 18-Interfaces/Ex1801/src/model/service/TaxService.java
@@ -0,0 +1,7 @@
+package model.service;
+
+public interface TaxService {
+	
+	double tax(double amount);
+
+}
