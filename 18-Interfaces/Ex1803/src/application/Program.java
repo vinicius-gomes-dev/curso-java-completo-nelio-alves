0add359d65efaaac6e41de085ec88f3b1121e8de
Vinicius Soares <vinicius.gomes.dev@gmail.com>
2021-02-09 03:38:53 -0300

    Solution with Interface and abstract class

--- 18-Interfaces/Ex1803/src/application/Program.java
+++ 18-Interfaces/Ex1803/src/application/Program.java
@@ -2,14 +2,14 @@ package application;
 
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
+import model.entities.AbstractShape;
 import model.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Shape s1 = new Circle(Color.WHITE, 2.0);
-		Shape s2 = new Rectangle(Color.BLACK, 3.0, 4.0);
+		AbstractShape s1 = new Circle(Color.WHITE, 2.0);
+		AbstractShape s2 = new Rectangle(Color.BLACK, 3.0, 4.0);
 		
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()) );
--- 18-Interfaces/Ex1803/src/model/entities/Shape.java
+++ 18-Interfaces/Ex1803/src/model/entities/Shape.java
@@ -1,26 +1,7 @@
 package model.entities;
 
-import model.enums.Color;
-
-public abstract class Shape {
+public interface Shape {
 	
-	private Color color;
-	
-	public Shape() {
-	}
-	
-	public Shape(Color color) {
-		this.color = color;
-	}
-	
-	public Color getColor() {
-		return color;
-	}
-	
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	public abstract Double area();
+	Double area();
 
 }
