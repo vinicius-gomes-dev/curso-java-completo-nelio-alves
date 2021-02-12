040ea7e7c2673a0fdb2320590640874d7a7c1dae
Vinicius Soares <vinicius.gomes.dev@gmail.com>
2021-02-12 06:42:41 -0300

    Solution with Comparable Interface on class Employee

--- 18-Interfaces/Ex1805/src/application/Program.java
+++ 18-Interfaces/Ex1805/src/application/Program.java
@@ -6,28 +6,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+
+import model.entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		String path = "./input.txt";
-		List<String> names = new ArrayList<>();
+		List<Employee> employees = new ArrayList<>();
 
 		try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
-			String field = bf.readLine();
+			String employeeCsv = bf.readLine();
 
-			while (field != null) {
-				names.add(field);
-				field = bf.readLine();
+			while (employeeCsv != null) {
+				String[] field = employeeCsv.split(",");
+				employees.add(new Employee(field[0], Double.parseDouble(field[1])));
+				employeeCsv = bf.readLine();
 			}
 
-			names.forEach(name -> System.out.println(name));
+			employees.forEach(employee -> System.out.println(employee));
 			System.out.println();
 
-			Collections.sort(names);
+			Collections.sort(employees);
 			
 			System.out.println("==> SORTED <==");
-			names.forEach(name -> System.out.println(name));
+			employees.forEach(employee -> System.out.println(employee));
 		} 
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
